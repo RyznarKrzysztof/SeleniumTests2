14433dff679eafcb7722cf70e5d2ae8d54bc929f
Krzysztof Ryznar <kryznar@sii.pl>
2018-10-21 21:02:55 +0200

    update

---
+++ src/test/java/BasicTest.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeMethod;
+
+public class BasicTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "src/test/drivers/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("disable-extensions");
+        driver = new ChromeDriver(options);
+    }
+
+//    @AfterMethod
+//    public void quitDriver(){
+//        driver.quit();
+//    }
+
+}
---
+++ src/test/java/SeleniumTests.java
@@ -0,0 +1,10 @@
+
+public class SeleniumTests extends BasicTest {
+
+
+
+
+
+
+
+}
